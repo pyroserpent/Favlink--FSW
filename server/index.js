50f005e38c347d7aa09a58d3c8618e67c385794a
pyroserpent <zacafnan@gmail.com>
2023-12-06 21:50:01 -0500

    modiefied index.js

--- server/index.js
+++ server/index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const clientPath = path.join(__dirname, 'client/dist');
+
+// Set the path for the client files
+const clientPath = path.join(__dirname, '..', 'client/dist');
 app.use(express.static(clientPath));
 
 app.get('/', (req, res) => {
+    // Serve the index.html file
     res.sendFile(path.join(clientPath, 'index.html'));
 });
 
+// Start the server
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
