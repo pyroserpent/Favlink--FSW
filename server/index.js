55fe09ac6acebeb0cb3fabc29f9c0b985b5778a2
pyroserpent <zacafnan@gmail.com>
2023-12-11 19:45:03 -0500

    postgres and db changes

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 3000;
+const pool = require('./db');
+
 
 // Set the path for the client files
 const clientPath = path.join(__dirname, '..', 'client/dist');
@@ -11,8 +13,18 @@ app.get('/', (req, res) => {
     // Serve the index.html file
     res.sendFile(path.join(clientPath, 'index.html'));
 });
+app.get('/favlinks', async (req, res) => {
+    try {
+      const allLinks = await pool.query("SELECT * FROM favlinks");
+      res.json(allLinks.rows);
+    } catch (err) {
+      console.error(err.message);
+    }
+  });
+  
 
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
