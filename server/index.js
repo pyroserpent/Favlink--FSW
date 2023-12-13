558042a0fb6125a8c064a5d7417837a1aff310a0
pyroserpent <zacafnan@gmail.com>
2023-12-12 23:27:23 -0500

    removed redundancy code

--- server/index.js
+++ server/index.js
@@ -3,12 +3,12 @@ const db = require('./db');
 const path = require('path');
 const app = express();
 const PORT = 3000;
-const pool = require('./db');
 
+app.use(express.json()); // This should be placed before your routes
 
+// API Routes
 app.get('/api/links', db.getLinks);
 app.post('/api/links', db.addLink);
-app.use(express.json());
 app.put('/api/links/:id', db.updateLink);
 app.delete('/api/links/:id', db.deleteLink);
 
@@ -20,60 +20,8 @@ app.get('/', (req, res) => {
     // Serve the index.html file
     res.sendFile(path.join(clientPath, 'index.html'));
 });
-app.get('/favlinks', async (req, res) => {
-    try {
-      const allLinks = await pool.query("SELECT * FROM favlinks");
-      res.json(allLinks.rows);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-  
 
 // Start the server
-
-app.post('/api/links', async (req, res) => {
-    try {
-      const { name, url } = req.body;
-      const newLink = await pool.query("INSERT INTO favlinks (name, url) VALUES ($1, $2) RETURNING *", [name, url]);
-      res.json(newLink.rows[0]);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-  app.put('/api/links/:id', async (req, res) => {
-    try {
-      const { id } = req.params; // Get the ID from the URL
-      const { name, url } = req.body; // Get the new name and URL from the request body
-  
-      const updateLink = await pool.query(
-        "UPDATE favlinks SET name = $1, url = $2 WHERE id = $3 RETURNING *",
-        [name, url, id]
-      );
-  
-      if (updateLink.rows.length === 0) {
-        return res.status(404).json("Link not found");
-      }
-  
-      res.json(updateLink.rows[0]);
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-  app.delete('/api/links/:id', async (req, res) => {
-    try {
-      const { id } = req.params; // Get the ID from the URL
-      const deleteLink = await pool.query("DELETE FROM favlinks WHERE id = $1 RETURNING *", [id]);
-  
-      if (deleteLink.rows.length === 0) {
-        return res.status(404).json("Link not found");
-      }
-  
-      res.json("Link was deleted");
-    } catch (err) {
-      console.error(err.message);
-    }
-  });
-  app.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
-});   
+});
