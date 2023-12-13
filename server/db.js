d2b654dc54aad2c4213cd7482c88c75e9f283729
pyroserpent <zacafnan@gmail.com>
2023-12-12 23:09:13 -0500

    changes to db.js

--- server/db.js
+++ server/db.js
@@ -1,20 +1,23 @@
 const { Pool } = require('pg');
+
 const pool = new Pool({
-  user: 'postgres',    // Replace with your PostgreSQL username
+  user: 'postgres',    
   host: 'localhost',
   database: 'linksapi',    // The name of your database
   password: 'afnan', // Replace with your PostgreSQL password
   port: 5432,              // The port PostgreSQL is running on (default is 5432)
 });
-module.exports = pool;
+
 const getLinks = (req, res) => {
-    pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, results) => {
-      if (error) {
-        throw error;
-      }
-      res.status(200).json(results.rows);
-    });
-  };
-  module.exports = {
-    getLinks,
-  };
+  pool.query('SELECT * FROM favlinks ORDER BY id ASC', (error, results) => {
+    if (error) {
+      throw error;
+    }
+    res.status(200).json(results.rows);
+  });
+};
+
+module.exports = {
+  pool,
+  getLinks,
+};
